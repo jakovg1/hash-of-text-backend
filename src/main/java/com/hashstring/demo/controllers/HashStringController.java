078b16ba0a85b59b5f9ab5e79284ed59dc4b5487
Jakov <jakov.gracin1@gmail.com>
2024-11-12 11:05:20 +0100

    Fix return codes in HashStringController and add exception handling

--- src/main/java/com/hashstring/demo/controllers/HashStringController.java
+++ src/main/java/com/hashstring/demo/controllers/HashStringController.java
@@ -1,9 +1,12 @@
 package com.hashstring.demo.controllers;
 
+import com.hashstring.demo.exceptions.NoSuchHashAlgorithmException;
 import com.hashstring.demo.model.responses.HashStringResponse;
 import com.hashstring.demo.model.responses.SupportedAlgorithmsResponse;
 import com.hashstring.demo.services.HashStringService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,23 +17,34 @@ public class HashStringController {
     private HashStringService hashStringService;
 
     @PostMapping("/")
-    public HashStringResponse getHash(@RequestBody String input) {
-        return this.hashStringService.getHashOfString(input);
+    public ResponseEntity<HashStringResponse> getHash(@RequestBody String input) {
+        if (input == null || input.isEmpty()) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        HashStringResponse response =  this.hashStringService.getHashOfString(input);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/setAlgorithm")
-    private void setHashAlgorithm(@RequestBody String algorithm) {
-        this.hashStringService.setHashAlgorithm(algorithm);
+    private ResponseEntity<String> setHashAlgorithm(@RequestBody String algorithm) {
+        try {
+            this.hashStringService.setHashAlgorithm(algorithm);
+            return ResponseEntity.ok("Algorithm set to: " + algorithm);
+        } catch (NoSuchHashAlgorithmException e) {
+            return ResponseEntity.badRequest().body("Invalid algorithm or algorithm not supported: " + algorithm);
+        }
     }
 
     @GetMapping("/getAlgorithm")
-    private String getHashAlgorithm(){
-        return this.hashStringService.getHashAlgorithm();
+    private ResponseEntity<String> getHashAlgorithm(){
+        String hashAlgorithm = this.hashStringService.getHashAlgorithm();
+        return ResponseEntity.ok(hashAlgorithm);
     }
 
     @GetMapping("/supportedAlgorithms")
-    private SupportedAlgorithmsResponse getSupportedHashAlgorithms() {
-        return this.hashStringService.getSupportedHashAlgorithms();
+    private ResponseEntity<SupportedAlgorithmsResponse> getSupportedHashAlgorithms() {
+        SupportedAlgorithmsResponse response = this.hashStringService.getSupportedHashAlgorithms();
+        return ResponseEntity.ok(response);
     }
 
 }
