8a226c060d6c7eb96ac881607bcb01a730b48834
Jakov <jakov.gracin1@gmail.com>
2024-11-12 12:37:28 +0100

    Add docs for controller, use swagger

--- src/main/java/com/hashstring/demo/controllers/HashStringController.java
+++ src/main/java/com/hashstring/demo/controllers/HashStringController.java
@@ -4,6 +4,10 @@ import com.hashstring.demo.exceptions.NoSuchHashAlgorithmException;
 import com.hashstring.demo.model.responses.HashStringResponse;
 import com.hashstring.demo.model.responses.SupportedAlgorithmsResponse;
 import com.hashstring.demo.services.HashStringService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,22 +15,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hash")
+@Tag(name = "Hash API", description = "API for generating hashes and managing hash algorithms")
 public class HashStringController {
 
     @Autowired
     private HashStringService hashStringService;
 
     @PostMapping("/")
+    @Operation(summary = "Generate hash", description = "Generates a hash of the provided string input using the currently selected algorithm.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully generated hash"),
+            @ApiResponse(responseCode = "400", description = "Invalid input or empty string")
+    })
     public ResponseEntity<HashStringResponse> getHash(@RequestBody String input) {
         if (input == null || input.isEmpty()) {
             return ResponseEntity.badRequest().body(null);
         }
-        HashStringResponse response =  this.hashStringService.getHashOfString(input);
+        HashStringResponse response = this.hashStringService.getHashOfString(input);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/setAlgorithm")
-    private ResponseEntity<String> setHashAlgorithm(@RequestBody String algorithm) {
+    @Operation(summary = "Set hash algorithm", description = "Sets the hash algorithm to be used for future hash generation. Supported algorithms include MD5, SHA-1, SHA-256, etc.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Algorithm set successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid algorithm or unsupported algorithm specified")
+    })
+    public ResponseEntity<String> setHashAlgorithm(@RequestBody String algorithm) {
         try {
             this.hashStringService.setHashAlgorithm(algorithm);
             return ResponseEntity.ok("Algorithm set to: " + algorithm);
@@ -36,17 +51,22 @@ public class HashStringController {
     }
 
     @GetMapping("/getAlgorithm")
-    private ResponseEntity<String> getHashAlgorithm(){
+    @Operation(summary = "Get current hash algorithm", description = "Retrieves the name of the currently selected hash algorithm.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved current algorithm")
+    })
+    public ResponseEntity<String> getHashAlgorithm() {
         String hashAlgorithm = this.hashStringService.getHashAlgorithm();
         return ResponseEntity.ok(hashAlgorithm);
     }
 
     @GetMapping("/supportedAlgorithms")
-    private ResponseEntity<SupportedAlgorithmsResponse> getSupportedHashAlgorithms() {
+    @Operation(summary = "Get supported algorithms", description = "Lists all supported hash algorithms that can be used for hash generation.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved supported algorithms")
+    })
+    public ResponseEntity<SupportedAlgorithmsResponse> getSupportedHashAlgorithms() {
         SupportedAlgorithmsResponse response = this.hashStringService.getSupportedHashAlgorithms();
         return ResponseEntity.ok(response);
     }
-
 }
-
-
